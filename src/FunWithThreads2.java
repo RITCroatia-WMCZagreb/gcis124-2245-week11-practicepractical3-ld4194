f9f3d199bc8b174212784718edf7162593014e51
Luka Doljanin (RIT Student) <ld4194@rit.edu>
2025-04-14 12:12:57 +0200

    problem solved

--- src/FunWithThreads2.java
+++ src/FunWithThreads2.java
@@ -5,17 +5,38 @@ public class FunWithThreads2 {
     public FunWithThreads2(){
         System.out.println("MAIN START");
         
+        Thread t1 = new Thread(new MyThread("Thread 1"));
+        Thread t2 = new Thread(new MyThread("Thread FunWithThreads2"));
+        t1.start();
+        t2.start();
+
         System.out.println("MAIN END");
     }
 
     public static void main(String[] args) throws Exception {
-        
         new FunWithThreads2();
-
     }
 
+    class MyThread implements Runnable{
 
-}
+        private String name="";
+        public MyThread(String name){
+            this.name = name;
+        }
+        @Override
+        public void run() {
+            for(int i=0;i<10;i++){
+                System.out.println(this.name + " " + i);    
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+       
+    }}
 
 /*
  * 
@@ -24,25 +45,25 @@ public class FunWithThreads2 {
 Thread start:Thread 1
 Thread 1 0
 Thread 1 1
-Thread 1 2
+Thread 1 FunWithThreads2
 Thread 1 3
-Thread start:Thread 2
+Thread start:Thread FunWithThreads2
 Thread 1 4
-Thread 2 0
+Thread FunWithThreads2 0
 Thread 1 5
-Thread 2 1
-Thread 2 2
+Thread FunWithThreads2 1
+Thread FunWithThreads2 FunWithThreads2
 Thread 1 7
-Thread 2 3
+Thread FunWithThreads2 3
 Thread 1 8
-Thread 2 4
+Thread FunWithThreads2 4
 Thread 1 9
 Thread end:Thread 1
 Main Thread END
-Thread 2 5
-Thread 2 6
-Thread 2 7
-Thread 2 8
-Thread 2 9
-Thread end:Thread 2
+Thread FunWithThreads2 5
+Thread FunWithThreads2 6
+Thread FunWithThreads2 7
+Thread FunWithThreads2 8
+Thread FunWithThreads2 9
+Thread end:Thread FunWithThreads2
  */
